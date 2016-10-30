9db4d4a3a021b6b757560b8f9c1d83605164715f
Anthony <anthony@linuxacademy.com>
2016-10-30 20:21:17 +0000

    Added basic Apache cookbook

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: apache
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+#Install apache package
+package 'apache2' do
+	package_name 'httpd'
+	action :install
+end
+
+service 'apache2' do
+	service_name 'httpd'
+	action [:start, :enable]
+end
+
+
